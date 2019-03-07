3a98eaa35f1fb39a26c427f50458fa90fe0859b0
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2019-03-07 18:18:14 +0200

    Functional updating

--- test.php
+++ test.php
@@ -1,8 +1,22 @@
 <?php
 
-$db = new Dataset ('tmp.db');
-$db->set (1, 2);
-$db->save ();
-$db->close ();
+class Nani
+{
+    public function save ()
+    {
+        $db = new Dataset ('tmp.db');
+        $db->set (1, 2);
+        $db->save ();
+        $db->close ();
+    }
+
+    public function savent ()
+    {
+        $db = new Dataset ('tmp.db');
+        $db->remove (1);
+        $db->save ();
+        $db->close ();
+    }
+}
 
 ?>
