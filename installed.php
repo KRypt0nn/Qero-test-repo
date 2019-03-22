b947b1fc66fb44443c4f441dd56ea4d8048169af
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2019-03-22 20:25:42 +0200

    upd

---
+++ installed.php
@@ -0,0 +1,27 @@
+<?php
+
+use Qero\Printer\Printer;
+use Qero\PackagesManager\PackagesManager;
+
+Printer::say ('
+
+Thank for installing [Qero test project]!
+Author: Podvirnyy Nikita
+
+Are you confirm license agreements? [Y/N]
+> ');
+
+$input = fopen ('php://stdin', 'r');
+
+while (array_search ($status = stream_get_contents ($input, 1), array ('Y', 'N', 'y', 'n')) === false);
+
+fclose ($input);
+
+if (strtolower ($status) == 'n')
+{
+    Printer::say ("\nOk :<");
+
+    (new PackagesManager)->deletePackage ('KRypt0nn/Qero-test-repo');
+}
+
+?>
