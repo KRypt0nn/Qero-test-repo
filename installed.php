19191a61467c7954257427f8a61642ecca4733cd
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2020-01-04 15:55:13 +0200

    upd

--- installed.php
+++ installed.php
@@ -1,34 +1,5 @@
 <?php
 
-use Qero\PackagesManager\PackagesManager;
+echo '   Thank for installing'. PHP_EOL . PHP_EOL;
 
-fwrite (STDOUT, '    '. implode (PHP_EOL .'    ', array (
-    'Thank for installing [Qero test project]!',
-    'Author: Podvirnyy Nikita',
-    '',
-    'Are you confirm license agreements? [Y/N]',
-    '> '
-)));
-
-while (true)
-{
-    $status = strtolower (stream_get_contents ($input = fopen ('php://stdin', 'r'), 1));
-    fclose ($input);
-
-    if (array_search ($status, array ('y', 'n')) === false)
-        fwrite (STDOUT, '   You pressed unknown symbol. Please, retry:
-    > ');
-
-    else break;
-}
-
-if ($status == 'n')
-{
-    global $controller;
-
-    fwrite (STDOUT, "\n    Ok :<\n");
-
-    $controller->manager->removePackage ('KRypt0nn/Qero-test-repo');
-}
-
-fwrite (STDOUT, PHP_EOL);
+unlink (__FILE__);
--- test.php
+++ test.php
@@ -1,28 +1,6 @@
 <?php
 
-MathLib\includeModule ('Math/AdditionalMath');
-
-use MathLib\AdditionalMath;
-
-class Encrypter
+function test (int $a, int $b): int
 {
-    public static function encode ($text, $key)
-    {
-        $enc = array ();
-
-        foreach (str_split ($text) as $char)
-            $enc[] = MathLib\AdditionalMath\pow (ord ($char), $key);
-
-        return implode (' ', $enc);
-    }
-
-    public static function decode ($text, $key)
-    {
-        $dec = '';
-
-        foreach (explode (' ', $text) as $char)
-            $dec .= MathLib\AdditionalMath\root ($char, $key);
-
-        return $dec;
-    }
+    return $a + $b;
 }
